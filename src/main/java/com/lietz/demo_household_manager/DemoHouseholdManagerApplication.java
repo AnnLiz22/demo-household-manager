8a790e8bfaa0b46a9e96e245344a70ef47870709
AnnLiz22 <bielakam@gmail.com>
2025-02-24 22:17:36 +0100

    first commit

---
+++ src/main/java/com/lietz/demo_household_manager/DemoHouseholdManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lietz.demo_household_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHouseholdManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHouseholdManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lietz/demo_household_manager/DemoHouseholdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lietz.demo_household_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHouseholdManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
