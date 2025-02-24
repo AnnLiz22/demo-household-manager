02c4d750223e33c519032d8b3dfe15429ad9fd50
AnnLiz22 <bielakam@gmail.com>
2025-02-24 22:51:31 +0100

    added fields to comment

--- src/main/java/com/lietz/demo_household_manager/model/Comment.java
+++ src/main/java/com/lietz/demo_household_manager/model/Comment.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -14,5 +16,20 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 
 }
