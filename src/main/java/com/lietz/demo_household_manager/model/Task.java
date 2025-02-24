03dface1191b7a91547f8387fe4a491bb387e976
AnnLiz22 <bielakam@gmail.com>
2025-02-24 22:48:10 +0100

    added fields for task; added relations

--- src/main/java/com/lietz/demo_household_manager/model/Task.java
+++ src/main/java/com/lietz/demo_household_manager/model/Task.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -13,6 +16,31 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title;
+    private String description;
 
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+    @ManyToOne
+    @JoinColumn(name = "executor_id")
+    private User executor;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime dueDate;
+    private LocalDateTime completedAt;
+    private boolean completed;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<Comment> comments;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
 
