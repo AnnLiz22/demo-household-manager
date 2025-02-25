cce9fcfe117c226653e2b398ada300e3371562eb
AnnLiz22 <bielakam@gmail.com>
2025-02-25 13:04:19 +0100

    added method for finding comments by user

--- src/main/java/com/lietz/demo_household_manager/repository/CommentRepository.java
+++ src/main/java/com/lietz/demo_household_manager/repository/CommentRepository.java
@@ -2,10 +2,13 @@ package com.lietz.demo_household_manager.repository;
 
 import com.lietz.demo_household_manager.model.Comment;
 import com.lietz.demo_household_manager.model.Task;
+import com.lietz.demo_household_manager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByTask(Task task);
+
+    List<Comment> findByUser(User user);
 }
